b2cff97a6a391f9f043c586e9fc45466f75852a3
Neuville <otiendeneville@gmail.com>
2026-07-23 18:33:18 +0300

    adding the hero image

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <main>
        <div className="partern"/>
        <div className='wrapper'>
+        <img src="./hero.png" alt="Hero Banner"/>
          <header>
           <h1>Find The <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hussle</h1>
          </header>
