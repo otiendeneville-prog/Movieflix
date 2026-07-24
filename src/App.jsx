488e466cc9d1beb52962bca6cff874e4e88e0fd5
Neuville <otiendeneville@gmail.com>
2026-07-24 16:19:01 +0300

    add the new api link

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect,useState } from 'react'
 import Search from './components/Search.jsx'
 
 
-const API_BASE_URL = 'https://api.themoviedb.org/3'
+const API_BASE_URL = 'https://api.themoviedb.org/3/'
   const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
   const API_OPTIONS={
     method:'GET',
@@ -15,16 +15,13 @@ const API_BASE_URL = 'https://api.themoviedb.org/3'
 function App() {
   const[searchTerm, setSearchTerm] = useState("");
   const[errorMessage, setErrorMessage] =useState("")
-const [movieList, setMovieList] = useState([]);
-setMovieList(data.results || []);
   const fetchMovies= async ()=>{
     try{
       const endpoint=`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
-      const response = await fetch(endpoint,API_OPTIONS)
-      
+      const response = await fetch('https://api.themoviedb.org/3/')
+      const data = await response.json();
       if(!response.ok){
         throw new Error('Failed to fetch movies')
-        const data = await response.json();
         
       }
     }catch(error){
