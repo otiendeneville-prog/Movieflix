2d749c07196e42771f61b07898a6202f38d92480
Neuville <otiendeneville@gmail.com>
2026-07-24 12:28:23 +0300

    Fresh Local Start

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import Search from './components/Search.jsx'
+
+function App() {
+  const[searchTerm, setSearchTerm] = useState("");
+  return (
+    <main>
+       <div className="partern"/>
+       <div className='wrapper'>
+        <img src="./hero.png" alt="Hero Banner"/>
+         <header>
+          <h1>Find The <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hussle</h1>
+         </header>
+         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+         <h1 className='text-white'>{searchTerm}</h1>
+       </div>
+    </main>
+  )
+}
+
+export default App
