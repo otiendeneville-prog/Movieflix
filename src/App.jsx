e25a6874a5586c3ff8c35657071cfb7ca5bcaa34
Neuville <otiendeneville@gmail.com>
2026-07-28 16:33:15 +0300

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   const fetchMovies= async ()=>{
     try{
       const endpoint=`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
-      const response = await fetch('https://api.themoviedb.org/3/')
+      const response = await fetch('endpoint,API_OPTIONS');
       const data = await response.json();
       if(!response.ok){
         throw new Error('Failed to fetch movies')
