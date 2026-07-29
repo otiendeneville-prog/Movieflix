998a27fa49e6ca82c079538aab74a2e48c801e9a
Neuville <otiendeneville@gmail.com>
2026-07-29 12:17:28 +0300

    add the star icon

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,8 +6,17 @@ function MovieCard({movie :
   return (
     <div className='movie-card'>
         <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}`:
-      '/no-movie.png'
-    }  />
+      '/no-movie.png' 
+    } alt={title} />
+    <div className='mt-4'>
+        <h1>{title}</h1>
+        <div className='content'>
+            <div className='rating'>
+                <img src="star.svg" alt="Star Icon" />
+            </div>
+
+        </div>
+    </div>
         <p className='text-white'>{title}</p>
     </div>
   )
