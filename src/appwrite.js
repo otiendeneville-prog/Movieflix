2c337ea36e7152d9f43271ad5a8001222dcbbdd0
Neuville <otiendeneville@gmail.com>
2026-08-03 07:25:05 +0300

    adding the asy function of the update searh count

--- src/appwrite.js
+++ src/appwrite.js
@@ -2,4 +2,7 @@ const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID =import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const TABLE_ID = import.meta.env.VITE_APPWRITE_TABLE_ID;
 
-console.log(PROJECT_ID,DATABASE_ID,TABLE_ID);
+
+export const updateSearchCount = async() =>{
+console.log(PROJECT_ID,DATABASE_ID,TABLE_ID);    
+}
