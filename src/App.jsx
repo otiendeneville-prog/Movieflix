0b3c48300db9ce0415df18c429d477fc572500fd
Neuville <otiendeneville@gmail.com>
2026-08-04 12:09:58 +0300

    import client database

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React, { useEffect,useState } from 'react'
 import Search from './components/Search.jsx'
 import Spinner from './Spinner.jsx';
 import MovieCard from './components/MovieCard.jsx';
-import { getTrendingMovies, updateSearchCount } from './appwrite.js';
+import { getTrendingMovies, updateSearchCount } from './appwrite.js'
+import { Databases,Client } from 'appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3/'
   const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
