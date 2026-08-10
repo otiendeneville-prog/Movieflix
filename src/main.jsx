6f011a5ff7ea4a669b421636a386d0750ae6869b
Neuville <otiendeneville@gmail.com>
2026-08-10 15:24:27 +0300

    import browser router

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import {creatBrowserRouter} from 'react-router';
+import { RouterProvider } from 'react-router';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
