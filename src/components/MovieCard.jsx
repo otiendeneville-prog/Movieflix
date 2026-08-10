210817b3addf8201db299ec20df66d756883aa9c
Neuville <otiendeneville@gmail.com>
2026-08-10 21:55:05 +0300

    import react-router-dom

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { BrowserRouter } from 'react-router-dom';
 function MovieCard({movie :
     {title,vote_average,poster_path,release_date,original_language}}) 
 {
