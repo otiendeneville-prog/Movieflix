a152573079b5ad427aab37b5bc17369d3190b51e
Neuville <otiendeneville@gmail.com>
2026-08-10 15:39:51 +0300

    install react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, StrictMode } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
